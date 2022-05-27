9f2d0279d90a5e1f9b1e9c1d373e9bac6bfbdf42
2637309949@qq.com <2637309949@qq.com>
2022-05-27 15:39:46 +0800

    style: 代码格式

--- main.go
+++ main.go
@@ -39,26 +39,32 @@ func main() {
 		web.Swagger("handler"))
 
 	//User routes
-	r.Handle(http.MethodPost, "/api/v1/user/login", h.Login)
-	r.Handle(http.MethodGet, "/api/v1/user/logout", h.Logout)
-	r.Handle(http.MethodPost, "/api/v1/user/register", h.Register)
-	r.Handle(http.MethodPost, "/api/v1/user/updatePassword", h.UpdatePassword) //for updatePassword
-	//
-	r.Handle(http.MethodPost, "/api/v1/user/sendVerificationEmail", h.SendVerificationEmail) //for markVerified email, send verification token
-	r.Handle(http.MethodPost, "/api/v1/user/verifyEmail", h.VerifyEmail)                     //for verify verification token and marked account
-	//
-	r.Handle(http.MethodPost, "/api/v1/user/sendPasswordResetEmail", h.SendPasswordResetEmail) //for sendPasswordResetEmail
-	r.Handle(http.MethodPost, "/api/v1/user/resetPassword", h.ResetPassword)                   //for resetPassword
+	{
+		r.Handle(http.MethodPost, "/api/v1/user/login", h.Login)
+		r.Handle(http.MethodGet, "/api/v1/user/logout", h.Logout)
+		r.Handle(http.MethodPost, "/api/v1/user/register", h.Register)
+		r.Handle(http.MethodPost, "/api/v1/user/updatePassword", h.UpdatePassword) //for updatePassword
+		//
+		r.Handle(http.MethodPost, "/api/v1/user/sendVerificationEmail", h.SendVerificationEmail) //for markVerified email, send verification token
+		r.Handle(http.MethodPost, "/api/v1/user/verifyEmail", h.VerifyEmail)                     //for verify verification token and marked account
+		//
+		r.Handle(http.MethodPost, "/api/v1/user/sendPasswordResetEmail", h.SendPasswordResetEmail) //for sendPasswordResetEmail
+		r.Handle(http.MethodPost, "/api/v1/user/resetPassword", h.ResetPassword)                   //for resetPassword
+	}
 
 	//Auth routes
-	r.Handle(http.MethodPost, "/api/v1/token/refresh", h.Refresh)
+	{
+		r.Handle(http.MethodPost, "/api/v1/token/refresh", h.Refresh)
+	}
 
 	//Article routes
-	r.Handle(http.MethodPost, "/api/v1/article", middles.AuthMiddleware(h.TokenValid), h.InsertArticle)
-	r.Handle(http.MethodGet, "/api/v1/articles", middles.CachePage(h.Cache, time.Minute), h.QueryArticle)
-	r.Handle(http.MethodGet, "/api/v1/article/:id", middles.CachePage(h.Cache, time.Minute), h.QueryArticleDetail)
-	r.Handle(http.MethodPut, "/api/v1/article/:id", middles.AuthMiddleware(h.TokenValid), h.UpdateArticle)
-	r.Handle(http.MethodDelete, "/api/v1/article/:id", middles.AuthMiddleware(h.TokenValid), h.DeleteArticle)
+	{
+		r.Handle(http.MethodPost, "/api/v1/article", middles.AuthMiddleware(h.TokenValid), h.InsertArticle)
+		r.Handle(http.MethodGet, "/api/v1/articles", middles.CachePage(h.Cache, time.Minute), h.QueryArticle)
+		r.Handle(http.MethodGet, "/api/v1/article/:id", middles.CachePage(h.Cache, time.Minute), h.QueryArticleDetail)
+		r.Handle(http.MethodPut, "/api/v1/article/:id", middles.AuthMiddleware(h.TokenValid), h.UpdateArticle)
+		r.Handle(http.MethodDelete, "/api/v1/article/:id", middles.AuthMiddleware(h.TokenValid), h.DeleteArticle)
+	}
 
 	//start
 	logger.Infof(context.TODO(), "listen and serve on 0.0.0.0:%v", viper.GetString("http.port"))
