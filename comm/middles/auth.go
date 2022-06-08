484ee10fc2984ff130ceb0425e859c565d9df668
2637309949@qq.com <2637309949@qq.com>
2022-06-08 11:49:47 +0800

    style: code

--- comm/middles/auth.go
+++ comm/middles/auth.go
@@ -3,8 +3,8 @@ package middles
 import "github.com/gin-gonic/gin"
 
 //AuthMiddleware ...
-func AuthMiddleware(authMiddle gin.HandlerFunc) gin.HandlerFunc {
+func AuthMiddleware(auth gin.HandlerFunc) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		authMiddle(ctx)
+		auth(ctx)
 	}
 }
